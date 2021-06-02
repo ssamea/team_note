0afab59734252df44136591e80f73c9d673e671d
ssamea <wlstjdals3@gmail.com>
2021-06-03 00:16:24 +0900

    upload 2021.06.03

---
+++ DP/LDS.py
@@ -0,0 +1,12 @@
+# LDS: 최장감소수열
+# 감소하는 부분
+n = int(input()) # 수열의 크기
+
+arr = list(map(int, input().split())) # 수열 입력
+
+dp = [1]*n
+
+for i in range(n-1,-1,-1):
+    for j in range(n-1,i,-1):
+        if arr[i] > arr[j]:
+            dp[i]=max(dp[i],dp[j]+1)
