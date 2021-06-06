d019e5e377e9b2cbb7f4b9fd4be56a9357292403
ssamea <wlstjdals3@gmail.com>
2021-06-06 19:50:59 +0900

    upload 2021.06.06

--- DP/LDS.py
+++ DP/LDS.py
@@ -6,7 +6,7 @@ arr = list(map(int, input().split())) # 수열 입력
 
 dp = [1]*n
 
-for i in range(n-1,-1,-1):
+for i in range(n-1, -1, -1):
     for j in range(n-1,i,-1):
         if arr[i] > arr[j]:
-            dp[i]=max(dp[i],dp[j]+1)
+            dp[i] = max(dp[i], dp[j]+1)
